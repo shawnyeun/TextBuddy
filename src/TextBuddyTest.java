962e107c84a3b562e8706fd66e6b62af89c83f02
shawnyeun <a0124181@nus.edu.sg>
2015-09-16 22:10:00 +0800

    testing junit test

--- src/TextBuddyTest.java
+++ src/TextBuddyTest.java
@@ -11,14 +11,17 @@ public class TextBuddyTest {
 		assertSame("test the number of counts of a word", 1, TextBuddy.searchFile("textfile.txt", "aaa"));
 	}
 	
+	@Test
 	public void testSortFile() throws IOException {
 		assertEquals("Compare strings", true, TextBuddy.sortFile("textfile.txt") );
 	}
 	
+	@Test
 	public void testAddCommand() throws IOException {
 		assertSame("test if string is added", "eee", TextBuddy.addCommand("textfile.txt", "eee"));
 	}
 	
+	@Test
 	public void testClearFile() throws IOException {
 		assertEquals("Tests if file is cleared", true, TextBuddy.clearFile("textfile.txt"));
 	}
